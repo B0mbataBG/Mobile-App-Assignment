65f9ad177e95571f6d036871e64143d6587079ac
Plamen Bukov <p.bukov12@gmail.com>
2023-03-07 10:10:21 +0000

    Created the sign up screen but getting Bad Request

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert } from 'react-native';
+import { TextInput } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View } from 'react-native';
+import { Button, ScrollView } from 'react-native-web';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class SignUpScreen extends Component{
+
+
+  constructor(props){
+    super (props);
+    this.state ={
+      firstName: "",
+      lastName: "",
+      email: "",
+      signUp: ""
+    }
+  }
+
+    signUp = () =>{
+    //Validation here
+
+
+      return fetch ("http://localhost:3333/api/1.0.0/user", {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state)
+      })
+      .then((response) => {
+        if(response.status === 201){
+          return response.json()
+        }
+        else if(response.status === 400){
+          throw 'Failed validation';
+        }
+        else {
+          throw 'Something went wrong';
+        }
+      })
+      .then((responseJson) => {
+        console.log("User created ID:", responseJson);
+        this.props.navigation.navigate("Login");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+    }
+
+      render(){
+        return(
+          <ScrollView>
+            <TextInput
+              placeholder='Enter your first name'
+              onChangeText={(firstName) => this.setState({firstName})}
+              value={this.state.firstName}
+              style={{padding:5, borderWidth:1, margin:5}}
+            />
+            <TextInput
+              placeholder='Enter your last name'
+              onChangeText={(lastName) => this.setState({lastName})}
+              value={this.state.lastName}
+              style={{padding:5, borderWidth:1, margin:5}}
+            />
+            <TextInput
+              placeholder='Enter your first email'
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email}
+              style={{padding:5, borderWidth:1, margin:5}}
+            />
+            <TextInput
+              placeholder='Enter your first password'
+              onChangeText={(password) => this.setState({password})}
+              value={this.state.password}
+              style={{padding:5, borderWidth:1, margin:5}}
+            />
+            <Button 
+              title="Create an account"
+              onPress={() => this.signUp()}
+            />
+          </ScrollView>
+        )
+      }
+    }
+
+export default SignUpScreen;
